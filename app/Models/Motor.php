7ca49c1e0d37913c6d0ca7918cbe3d0e74f12f3b
gia linggardi <keongsiputcicak1@gmail.com>
2026-08-04 00:55:15 +0700

    test

--- app/Models/Motor.php
+++ app/Models/Motor.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Motor extends Model
 {
-    //
+    use HasFactory;
+
+    // Daftar kolom yang diizinkan untuk diisi dari form
+    protected $fillable = [
+        'merk_tipe',
+        'harga',
+        'tahun_kendaraan',
+        'kilometer',
+        'kondisi_kendaraan',
+        'kelengkapan_dokumen',
+        'foto',                 
+        'detail_spesifikasi',
+        'status_tayang'
+    ];
 }
